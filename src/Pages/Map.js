009d0471b9dc18e337406389bb2f76776006c134
Raghav <raghav.mehta.2004@gmail.com>
2024-12-13 07:08:05 -0500

    checking deployed api

--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -8,7 +8,7 @@ import { Card } from "@mui/material";
 
 // Create axios instance with default config
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://api-jemx.onrender.com",
   withCredentials: true,
 });
 
